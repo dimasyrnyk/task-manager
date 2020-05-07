332f7c657626a71199eb17976a8dd7ad61bbc6f7
Dimasyrnyk <syrnykdmitriy@gmail.com>
2020-05-07 16:50:25 +0300

    Updates

---
+++ precache-manifest.78af6c537d7a32958dbd0adff57ad46c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb2702f84e824a0b4438dc0d676c7b6",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "2a51f4bcbe499ede0cae",
+    "url": "/task-manager/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "9aa03f5b43b866f5560b",
+    "url": "/task-manager/static/css/main.d0ab65e0.chunk.css"
+  },
+  {
+    "revision": "2a51f4bcbe499ede0cae",
+    "url": "/task-manager/static/js/2.deed43e6.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/task-manager/static/js/2.deed43e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aa03f5b43b866f5560b",
+    "url": "/task-manager/static/js/main.25a97abe.chunk.js"
+  },
+  {
+    "revision": "ac2e9ea1a96596ecb557",
+    "url": "/task-manager/static/js/runtime-main.38ed45e5.js"
+  }
+]);
